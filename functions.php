e1f959b494db5788cc118dbf1ee02df110750e47
Pedro Belasco <pbelasco@gmail.com>
2013-12-19 16:36:53 -0200

    template novo de salas de imprensa

--- functions.php
+++ functions.php
@@ -82,6 +82,17 @@ function bones_register_sidebars() {
     	'before_title' => '<h4 class="widgettitle">',
     	'after_title' => '</h4>',
     ));
+
+    register_sidebar(array(
+        'id' => 'sidebar-salas-de-imprensa',
+        'name' => 'Barra lateral página de salas de imprensa',
+        'description' => 'Usado somente no templatre de salas de imprensa.',
+        'before_widget' => '<div id="%1$s" class="widget %2$s">',
+        'after_widget' => '</div>',
+        'before_title' => '<h4 class="widgettitle">',
+        'after_title' => '</h4>',
+    ));
+    
     
     /* 
     to add more sidebars or widgetized areas, just copy
---
+++ page-salas-de-imprensa.php
@@ -0,0 +1,77 @@
+<?php
+/*
+Template Name: Salas de imprensa
+*/
+?>
+
+<?php get_header(); ?>
+			
+			<div id="content" class="clearfix">
+			
+				<div id="main" class="twelve columns clearfix" role="main">
+
+					<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+					
+					<article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article">
+						
+						<header>
+							
+							<h1><?php the_title(); ?></h1>
+						
+						</header> <!-- end article header -->
+					
+						<section class="post_content">
+							<?php the_content(); ?>
+					
+						</section> <!-- end article section -->
+						
+						<footer>
+			
+							<p class="clearfix"><?php the_tags('<span class="tags">Tags: ', ', ', '</span>'); ?></p>
+							
+						</footer> <!-- end article footer -->
+					
+					</article> <!-- end article -->
+					
+					<?php comments_template(); ?>
+					
+					<?php endwhile; ?>	
+					
+					<?php else : ?>
+					
+					<article id="post-not-found">
+					    <header>
+					    	<h1>Não encontrado</h1>
+					    </header>
+					    <section class="post_content">
+					    	<p>Desculpe mas o que você procura não foi encontrado neste endereço.</p>
+					    </section>
+					    <footer>
+					    </footer>
+					</article>
+					
+					<?php endif; ?>
+			
+				</div> <!-- end #main -->
+    
+				<div id="sidebar-salas-de-imprensa" class="sidebar four columns" role="complementary">
+
+					<div class="panel">
+				
+						<?php if ( is_active_sidebar( 'sidebar-salas-de-imprensa' ) ) : ?>
+
+							<?php dynamic_sidebar( 'sidebar-salas-de-imprensa' ); ?>
+
+						<?php else : ?>
+							<!-- This content shows up if there are no widgets defined in the backend. -->
+							
+							<div class="alert-box">Nenhum Widget ativo.</div>
+
+						<?php endif; ?>
+
+					</div>
+
+				</div>
+			</div> <!-- end #content -->
+
+<?php get_footer(); ?>
